d5fa775c05c37598998b73cac646767a38e96a7e
ricardo <ricardotsopse@gmail.com>
2024-04-22 17:17:10 +0100

    slight modification on the get method

--- src/bucket/schema/bucket.schema.ts
+++ src/bucket/schema/bucket.schema.ts
@@ -1,19 +1,19 @@
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import mongoose, { Document, HydratedDocument } from 'mongoose';
-import { Message } from 'src/message/schema/message.schema';
+// import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+// import mongoose, { Document, HydratedDocument } from 'mongoose';
+// import { Message } from 'src/message/schema/message.schema';
 
-export type BucketDocument = HydratedDocument<Bucket>;
+// export type BucketDocument = HydratedDocument<Bucket>;
 
-@Schema()
-export class Bucket extends Document {
-  @Prop({ required: false })
-  title: string;
+// @Schema()
+// export class Bucket extends Document {
+//   @Prop({ required: false })
+//   title: string;
 
-  @Prop({ required: true })
-  creator_id: string;
+//   @Prop({ required: true })
+//   creator_id: string;
 
-  @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Message' }] })
-  message_ids: Message;
-}
+//   @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Message' }] })
+//   message_ids: Message;
+// }
 
-export const BucketSchema = SchemaFactory.createForClass(Bucket);
+// export const BucketSchema = SchemaFactory.createForClass(Bucket);
